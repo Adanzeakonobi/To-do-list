ae1748cb27e76e15ebf8b944134b75c8f4fc15d0
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-04-28 01:58:13 -0600

    update webdevpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
@@ -21,5 +25,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
